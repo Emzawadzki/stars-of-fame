4bd557d7d4da9b1bda8cf20788699b2a32cef01b
unknown <emzawadzki@gmail.com>
2019-05-19 14:39:47 +0200

    Updates

---
+++ precache-manifest.57227b30fc9851b33bf9c4886e0df8be.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5c666a83817dc6bae0bceb80ac143cf",
+    "url": "/stars-of-fame/index.html"
+  },
+  {
+    "revision": "dfa7790397f87b4f6300",
+    "url": "/stars-of-fame/static/css/main.113871f2.chunk.css"
+  },
+  {
+    "revision": "82f50f46894fea335385",
+    "url": "/stars-of-fame/static/js/2.63dfe4bd.chunk.js"
+  },
+  {
+    "revision": "dfa7790397f87b4f6300",
+    "url": "/stars-of-fame/static/js/main.0b27327f.chunk.js"
+  },
+  {
+    "revision": "5a920129474266acfbba",
+    "url": "/stars-of-fame/static/js/runtime~main.0b8014fc.js"
+  }
+]);
